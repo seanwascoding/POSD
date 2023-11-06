d75811198986d0b757ae31f07b8c91140da70229
Arno <oejk97614@gmail.com>
2023-11-06 15:57:38 +0800

    test

--- src/parser.h
+++ src/parser.h
@@ -23,24 +23,24 @@ public:
         while (!_scanner->isDone())
         {
             std::string token = _scanner->nextToken(); // (
-            _builder->buildOperand(_scanner->nextDouble()); // 10
-            std::string temp1 = _scanner->nextToken(); // +
-            _scanner->nextToken(); // (
-            _scanner->nextToken(); // (
-            _builder->buildOperand(_scanner->nextDouble()); // 11
-            std::string temp2 = _scanner->nextToken(); // +
-            _builder->buildOperand(_scanner->nextDouble()); // 12
-            _builder->buildExpression(temp1[0]);
-            _builder->endExpression(); // end
+            // _builder->buildOperand(_scanner->nextDouble()); // 10
+            // std::string temp1 = _scanner->nextToken(); // +
+            // _scanner->nextToken(); // (
+            // _scanner->nextToken(); // (
+            // _builder->buildOperand(_scanner->nextDouble()); // 11
+            // std::string temp2 = _scanner->nextToken(); // +
+            // _builder->buildOperand(_scanner->nextDouble()); // 12
+            // _builder->buildExpression(temp1[0]);
+            // _builder->endExpression(); // end
 
-            temp2 = _scanner->nextToken(); // *
-            _scanner->nextToken(); // (
-            _builder->buildOperand(_scanner->nextDouble()); // 5
-            std::string temp3 = _scanner->nextToken(); // *
-            _builder->buildOperand(_scanner->nextDouble()); // 4
-            _builder->endExpression(); // end
-            _builder->endExpression(); // end
-            _builder->endExpression(); // end
+            // temp2 = _scanner->nextToken(); // *
+            // _scanner->nextToken(); // (
+            // _builder->buildOperand(_scanner->nextDouble()); // 5
+            // std::string temp3 = _scanner->nextToken(); // *
+            // _builder->buildOperand(_scanner->nextDouble()); // 4
+            // _builder->endExpression(); // end
+            // _builder->endExpression(); // end
+            // _builder->endExpression(); // end
         }
     }
 
