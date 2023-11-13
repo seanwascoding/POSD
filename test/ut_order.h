103ac4e719db19e34509439f83b0e4028672798a
seanwascoding <a225108347171@gmail.com>
2023-11-13 20:30:14 +0800

    final version

--- test/ut_order.h
+++ test/ut_order.h
@@ -38,17 +38,26 @@ TEST(order, normal)
         folder.add(&visitor);
         folder.add(new File("structure/file.txt"));
 
-        string temp;
-        it = folder.createIterator(OrderBy::Normal);
-        for (it->first(); !it->isDone(); it->next())
-        {
-            cout << it->currentItem()->name() << endl;
-            temp += it->currentItem()->name();
-            temp += "\n";
-        }
+        TreeVisitor *v = new TreeVisitor(OrderBy::Normal);
+        folder.accept(v);
+        // cout << v->getTree();
 
-        ASSERT_TRUE(it->isDone());
-        ASSERT_EQ("home\nvisitor\nfile.txt\n", temp);
+        ASSERT_EQ(".\n"
+                  "├── home\n"
+                  "│   ├── hello.txt\n"
+                  "│   ├── my_profile\n"
+                  "│   ├── Documents\n"
+                  "│   │   └── favorites\n"
+                  "│   │       ├── clean-architecture.pdf\n"
+                  "│   │       ├── cqrs.pdf\n"
+                  "│   │       └── domain-driven-design.pdf\n"
+                  "│   └── Downloads\n"
+                  "│       └── funny.png\n"
+                  "├── visitor\n"
+                  "│   ├── file1.txt\n"
+                  "│   └── file2.txt\n"
+                  "└── file.txt\n",
+                  v->getTree());
     }
     catch (const char *e)
     {
@@ -91,16 +100,26 @@ TEST(order, OrderByNameIterator)
         folder.add(&visitor);
         folder.add(new File("structure/file.txt"));
 
-        string temp;
-        it = folder.createIterator(OrderBy::Name);
-        for (it->first(); !it->isDone(); it->next())
-        {
-            cout << it->currentItem()->name() << endl;
-            temp += it->currentItem()->name();
-            temp += "\n";
-        }
-        ASSERT_TRUE(it->isDone());
-        ASSERT_EQ("file.txt\nhome\nvisitor\n", temp);
+        TreeVisitor *v = new TreeVisitor(OrderBy::Name);
+        folder.accept(v);
+        // cout << v->getTree();
+
+        ASSERT_EQ(".\n"
+                  "├── file.txt\n"
+                  "├── home\n"
+                  "│   ├── Documents\n"
+                  "│   │   └── favorites\n"
+                  "│   │       ├── clean-architecture.pdf\n"
+                  "│   │       ├── cqrs.pdf\n"
+                  "│   │       └── domain-driven-design.pdf\n"
+                  "│   ├── Downloads\n"
+                  "│   │   └── funny.png\n"
+                  "│   ├── hello.txt\n"
+                  "│   └── my_profile\n"
+                  "└── visitor\n"
+                  "    ├── file1.txt\n"
+                  "    └── file2.txt\n",
+                  v->getTree());
     }
     catch (const char *e)
     {
@@ -143,16 +162,26 @@ TEST(order, OrderByNameWithFolderFirstIterator)
         folder.add(&visitor);
         folder.add(new File("structure/file.txt"));
 
-        string temp;
-        it = folder.createIterator(OrderBy::NameWithFolderFirst);
-        for (it->first(); !it->isDone(); it->next())
-        {
-            cout << it->currentItem()->name() << endl;
-            temp += it->currentItem()->name();
-            temp += "\n";
-        }
-        ASSERT_TRUE(it->isDone());
-        ASSERT_EQ("home\nvisitor\nfile.txt\n", temp);
+        TreeVisitor *v = new TreeVisitor(OrderBy::NameWithFolderFirst);
+        folder.accept(v);
+        // cout << v->getTree();
+
+        ASSERT_EQ(".\n"
+                  "├── home\n"
+                  "│   ├── Documents\n"
+                  "│   │   └── favorites\n"
+                  "│   │       ├── clean-architecture.pdf\n"
+                  "│   │       ├── cqrs.pdf\n"
+                  "│   │       └── domain-driven-design.pdf\n"
+                  "│   ├── Downloads\n"
+                  "│   │   └── funny.png\n"
+                  "│   ├── hello.txt\n"
+                  "│   └── my_profile\n"
+                  "├── visitor\n"
+                  "│   ├── file1.txt\n"
+                  "│   └── file2.txt\n"
+                  "└── file.txt\n",
+                  v->getTree());
     }
     catch (const char *e)
     {
@@ -196,63 +225,29 @@ TEST(order, OrderByKindIterator)
         folder.add(new File("structure/file.txt"));
         folder.add(&home);
 
-        // todo assert test
         TreeVisitor *v = new TreeVisitor(OrderBy::Kind);
         folder.accept(v);
-        cout << v->getTree();
-    }
-    catch (const char *e)
-    {
-        std::cerr << e << '\n';
-    }
-    delete it;
-}
+        // cout << v->getTree();
 
-TEST(order, visitor)
-{
-    TreeVisitor *visit = nullptr;
-    Iterator *it = nullptr;
-    try
-    {
-        Folder folder("structure");
-        Folder home("structure/home");
-        Folder visitor("structure/visitor");
-
-        Folder Documents("structure/home/Documents");
-        Folder favorites("structure/home/Documents/favorites");
-        favorites.add(new File("structure/home/Documents/favorites/clean-architecture.pdf"));
-        favorites.add(new File("structure/home/Documents/favorites/cqrs.pdf"));
-        favorites.add(new File("structure/home/Documents/favorites/domain-driven-design.pdf"));
-        Documents.add(&favorites);
-
-        Folder Downloads("structure/home/Downloads");
-        Downloads.add(new File("structure/home/Downloads/funny.png"));
-
-        home.add(new File("structure/home/hello.txt"));
-        home.add(new File("structure/home/my_profile"));
-        home.add(&Documents);
-        home.add(&Downloads);
-
-        visitor.add(new File("structure/visitor/file1.txt"));
-        visitor.add(new File("structure/visitor/file2.txt"));
-        Folder nested("structure/visitor/nested");
-        nested.add(new File("structure/visitor/nested/file3.txt"));
-        nested.add(new File("structure/visitor/nested/file4.txt"));
-        visitor.add(&nested);
-
-        folder.add(&home);
-        folder.add(&visitor);
-        folder.add(new File("structure/file.txt"));
-
-        visit = new TreeVisitor(OrderBy::Name);
-
-        folder.accept(visit);
-
-        string output = visit->getTree();
-
-        cout << output;
-
-        ASSERT_EQ(".\n\xE2\x94\x9C\xE2\x94\x80\xE2\x94\x80 file.txt\n\xE2\x94\x9C\xE2\x94\x80\xE2\x94\x80 home\n\xE2\x94\x82   \xE2\x94\x9C\xE2\x94\x80\xE2\x94\x80 Documents\n\xE2\x94\x82   \xE2\x94\x82   \xE2\x94\x94\xE2\x94\x80\xE2\x94\x80 favorites\n\xE2\x94\x82   \xE2\x94\x82       \xE2\x94\x9C\xE2\x94\x80\xE2\x94\x80 clean-architecture.pdf\n\xE2\x94\x82   \xE2\x94\x82       \xE2\x94\x9C\xE2\x94\x80\xE2\x94\x80 cqrs.pdf\n\xE2\x94\x82   \xE2\x94\x82       \xE2\x94\x94\xE2\x94\x80\xE2\x94\x80 domain-driven-design.pdf\n\xE2\x94\x82   \xE2\x94\x9C\xE2\x94\x80\xE2\x94\x80 Downloads\n\xE2\x94\x82   \xE2\x94\x82   \xE2\x94\x94\xE2\x94\x80\xE2\x94\x80 funny.png\n\xE2\x94\x82   \xE2\x94\x9C\xE2\x94\x80\xE2\x94\x80 hello.txt\n\xE2\x94\x82   \xE2\x94\x94\xE2\x94\x80\xE2\x94\x80 my_profile\n\xE2\x94\x94\xE2\x94\x80\xE2\x94\x80 visitor\n    \xE2\x94\x9C\xE2\x94\x80\xE2\x94\x80 file1.txt\n    \xE2\x94\x9C\xE2\x94\x80\xE2\x94\x80 file2.txt\n    \xE2\x94\x94\xE2\x94\x80\xE2\x94\x80 nested\n        \xE2\x94\x9C\xE2\x94\x80\xE2\x94\x80 file3.txt\n        \xE2\x94\x94\xE2\x94\x80\xE2\x94\x80 file4.txt\n", output);
+        ASSERT_EQ(".\n"
+                  "├── home\n"
+                  "│   ├── my_profile\n"
+                  "│   ├── Documents\n"
+                  "│   │   └── favorites\n"
+                  "│   │       ├── clean-architecture.pdf\n"
+                  "│   │       ├── cqrs.pdf\n"
+                  "│   │       └── domain-driven-design.pdf\n"
+                  "│   ├── Downloads\n"
+                  "│   │   └── funny.png\n"
+                  "│   └── hello.txt\n"
+                  "├── visitor\n"
+                  "│   ├── nested\n"
+                  "│   │   ├── file3.txt\n"
+                  "│   │   └── file4.txt\n"
+                  "│   ├── file1.txt\n"
+                  "│   └── file2.txt\n"
+                  "└── file.txt\n",
+                  v->getTree());
     }
     catch (const char *e)
     {
--- test/ut_parser.h
+++ test/ut_parser.h
@@ -27,11 +27,33 @@ TEST(parser, parser)
         parser->parse();
         Node *getNode = parser->getRoot();
 
-        TreeVisitor *visit = new TreeVisitor(OrderBy::NameWithFolderFirst);
+        TreeVisitor *visit = new TreeVisitor(OrderBy::Kind);
 
         getNode->accept(visit);
         string output = visit->getTree();
-        cout << output;
+        // cout << output;
+
+        ASSERT_EQ(".\n"
+                  "├── home\n"
+                  "│   ├── my_profile\n"
+                  "│   ├── Documents\n"
+                  "│   │   ├── favorites\n"
+                  "│   │   │   ├── clean-architecture.pdf\n"
+                  "│   │   │   ├── cqrs.pdf\n"
+                  "│   │   │   └── domain-driven-design.pdf\n"
+                  "│   │   ├── hello.txt\n"
+                  "│   │   └── note.txt\n"
+                  "│   ├── Downloads\n"
+                  "│   │   └── funny.png\n"
+                  "│   └── hello.txt\n"
+                  "├── visitor\n"
+                  "│   ├── nested\n"
+                  "│   │   ├── file3.txt\n"
+                  "│   │   └── file4.txt\n"
+                  "│   ├── file1.txt\n"
+                  "│   └── file2.txt\n"
+                  "└── file.txt\n",
+                  visit->getTree());
     }
     catch (const char *e)
     {
