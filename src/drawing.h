8e17f99f7cb18238e3952aac5871b8a715e45b24
seanwascoding <a225108347171@gmail.com>
2023-12-09 23:44:42 +0800

    test

--- src/drawing.h
+++ src/drawing.h
@@ -9,48 +9,52 @@
 class Drawing : public DomainObject
 {
 public:
-  Drawing(std::string id, Painter *painter) : DomainObject(id), _painter(painter)
-  {
-  }
+	Drawing(std::string id, Painter *painter) : DomainObject(id), _painter(painter)
+	{
+	}
 
-  Drawing(std::string id, Painter *painter, std::list<Shape *> shapes) : DomainObject(id), _painter(painter), _shapes(shapes)
-  {
-  }
+	Drawing(std::string id, Painter *painter, std::list<Shape *> shapes) : DomainObject(id), _painter(painter), _shapes(shapes)
+	{
+	}
 
-  Painter *painter() const
-  {
-    return _painter;
-  }
+	Painter *painter() const
+	{
+		std::cout << "painter" << std::endl;
+		return _painter;
+	}
 
-  void setPainter(Painter *painter)
-  {
-    _painter = painter;
-    UnitOfWork::instance()->registerDirty(this);
-  }
+	void setPainter(Painter *painter)
+	{
+		std::cout << "setPainter" << std::endl;
+		_painter = painter;
+		UnitOfWork::instance()->registerDirty(this);
+	}
 
-  Shape *getShape(int i) const
-  {
-    if (i < 0 || _shapes.size() == 0)
-      return nullptr;
+	Shape *getShape(int i) const
+	{
+		std::cout << "getShape" << std::endl;
+		if (i < 0 || _shapes.size() == 0)
+			return nullptr;
 
-    auto it = _shapes.begin();
-    for (int j = 0; j < i; j++)
-      it++;
+		auto it = _shapes.begin();
+		for (int j = 0; j < i; j++)
+			it++;
 
-    return *it;
-  }
+		return *it;
+	}
 
-  std::string getShapesAsString() const
-  {
-    std::string result = "";
-    for (auto shape : _shapes)
-    {
-      result += shape->toString() + "\n";
-    }
-    return result;
-  }
+	std::string getShapesAsString() const
+	{
+		std::cout << "getShapesAsString" << std::endl;
+		std::string result = "";
+		for (auto shape : _shapes)
+		{
+			result += shape->toString() + "\n";
+		}
+		return result;
+	}
 
 private:
-  Painter *_painter;
-  std::list<Shape *> _shapes;
+	Painter *_painter;
+	std::list<Shape *> _shapes;
 };
