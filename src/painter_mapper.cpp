cee8881bf7763be56f31a65ad271f24f3df20cb3
seanwascoding <a225108347171@gmail.com>
2023-12-10 17:30:54 +0800

    test

--- src/painter_mapper.cpp
+++ src/painter_mapper.cpp
@@ -39,9 +39,10 @@ void PainterMapper::del(std::string id)
 std::string PainterMapper::updateStmt(DomainObject *domainObject) const
 {
     std::cout << "PainterMapper::updateStmt" << std::endl;
-    Painter *painter = static_cast<Painter *>(domainObject);
-    std::string stmt = "UPDATE painter SET name='" + painter->name() + "' WHERE ID='" + painter->id() + "'";
+    Painter *painter = dynamic_cast<Painter *>(domainObject);
     std::cout << "PainterMapper::updateStmt-2" << std::endl;
+    std::string stmt = "UPDATE painter SET name='" + painter->name() + "' WHERE ID='" + painter->id() + "'";
+    std::cout << "PainterMapper::updateStmt-3" << std::endl;
     return stmt;
 }
 
